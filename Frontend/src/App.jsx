2c6e8abcc53c94d8ffc28ab0dd0575ddc453d2b3
Sachin <sachinsunny303@gmail.com>
2023-11-04 09:11:58 +0000

    frontend routing started

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Polling App</h1>
+        <nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="register">Register</Link>
+          </li>
+          <li>
+            <Link to="login">Login</Link>
+          </li>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
